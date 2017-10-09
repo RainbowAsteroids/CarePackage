6056c165b06c6302a690a80ccfa1ad82f4cf5099
Felix Zactor <buildermancampbell@aol.com>
2017-10-09 02:04:10 -0400

    Hashing
    A bunch of functions that should help you hash stuff!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CarePackage Module 1.1.1
+# CarePackage Module 1.1
 This is my coder base/module.
 ## Dependencies
 Python 3.4
@@ -9,7 +9,7 @@ At the start of your program (or where ever you declare your imports) put in thi
 
 Just need a single function from the program? Do this instead: `from CarePackage import [FUNCTION HERE]`
 
-**Make sure the** `CarePackage.py` **file is in the same directory as your program!**
+**Make sure the `CarePackage.py` file is in the same directory as your program!**
 
 # CarePackage Terminal 1.0
 This is a terminal you can have fun with when testing out some of the functions.
@@ -17,6 +17,4 @@ This is a terminal you can have fun with when testing out some of the functions.
 ## Dependencies
 CarePackage Module
 
-**Make sure the** `CarePackage.py` **file is in the same directory as** `terminal.py`**!**
-
-# For info on the functions, check out the wiki!
+**Make sure the `CarePackage.py` file is in the same directory as `terminal.py`!**
--- terminal.py
+++ terminal.py
@@ -1,5 +1,30 @@
 """Imports"""
 import CarePackage
+"""Classes"""
+class login:
+  def create():
+    while True:
+      username = input("What do you want your username to be? ")
+      if CarePackage.hasher.password.checkAccount(username):
+        print("Error! That username is taken!")
+      else:
+        break
+    CarePackage.hasher.password.save(username, input("What do you want your password to be? "))
+    CarePackage.clearScreen()
+  def login():
+    while True:
+      username = input("What is your username? ")
+      if CarePackage.hasher.password.checkAccount(username):
+        break
+      else:
+        print("Error! That user does not exist!")
+    while True:
+      password = input("What is your password?")
+      CarePackage.clearScreen()
+      if login(username, password):
+        print("Welcome", username+"!")
+      else:
+        print("Error! Wrong password!")
 """Functions"""
 def commandHandler(command):
 	command = command.split(" ")
@@ -7,6 +32,17 @@ def commandHandler(command):
 		helpText()
 	elif command[0] == "version"
 		version()
+	elif command[0] == "rainbow":
+		CarePackage.rainbow(command[1])
+	elif command[0] == "hash":
+	  if command[1] == "file":
+	    CarePackage.hasher.printout(open(command[2]))
+	  elif command[1] == "string":
+	    CarePackage.hasher.printout(command[2])
+	elif command[0] == "login":
+	  loginMode()
+	elif command[0] == "signoff":
+	  signOff()
 def version():
 	if CarePackage.importCl:
 		CarePackage.rainbow("CarePackage Terminal: 1.0")
@@ -20,9 +56,12 @@ def helpText():
 	print("""
 	Command list:
 	
-	help - Prints commands
-	version - Prints the version of this program
+	help - Prints helptext - help
+	version - Prints the version of this program - version
 	rainbow - Prints the desired text in rainbow. Requires click - rainbow [TEXT]
+	hash - Prints hashes of a string or file - hash <file/string> [FILENAME/TEXT]
+	login - Puts the terminal into login mode - login
+	signoff - Gives the signoff message - signoff
 	
 	As more functions are added to CarePackage, more cool commands will be available.
 	""")
@@ -33,12 +72,24 @@ def signOff():
 	else:
 		print("Happy Programming!")
 def leave()
+  CarePackage.clearScreen()
 	signOff()
 	exit()
+def loginMode():
+  while True:
+    choice = CarePackage.menu(["Login", "Create a New Account", "Delete Account"], exit=True)
+    if choice == 0:
+      break
+    elif choice == 1:
+      login.login(input())
+    elif choice == 2:
+      login.create()
 """Rest of the Program"""
+CarePackage.clearScreen()
 try:
 	while(True):
 		commandHandler(input(">>>"))
 except KeyboardInterrupt:
+  CarePackage.clearScreen()
 	signOff()
 	exit()
